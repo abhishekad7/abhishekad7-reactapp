5a5d5f8130dd03d0fd7f4d6d8b9cba32040fe3b3
abhishekad7 <abhishekad7@gmail.com>
2019-06-11 00:22:18 +0530

    Bugs fixed

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,88 +4,98 @@ import "./project.css"
 
 class Projects extends Component {
     
+    projects = [
+        {
+            title: "Future Trivia",
+            tools: ["Django", "Javascript"],
+            desc: "A Django based web app for onine quizzing and exams.",
+            sss: ["/img/ft1.jpg"],
+            navs:[
+                {"href": "https://github.com/abhishekad7/futureTrivia", "name": "See on Github"},
+                {"href": "https://futuretrivia.pythonanywhere.com/", "name": "Live"}
+            ]
+
+        },
+
+        {
+            title: "Kody Alarm: Coding Calendar",
+            tools: ["Java", "Android Studio"],
+            desc: "An android app which shows you all upcoming programming contests on AtCoder, Codeforces and Codechef. User can set reminder for upcoming contest.",
+            sss: ["/img/kody1.jpg"],
+            navs:[
+                {"href": "https://github.com/abhishekad7/coding_calendar", "name": "See on Github"},
+                {"href": "https://drive.google.com/open?id=1uPofiI-gQ2RiafWQLO3Q8CyZQRGk1lXe", "name": "Download App"}
+            ]
+
+        },
+
+        {
+            title: "This Webapp",
+            tools: ["ReactJS", "Javascript", "CSS"],
+            desc: "A ReactJS based web app. This web app is an image of me.",
+            sss: [],
+            navs:[
+                {"href": "https://github.com/abhishekad7/abhishekad7-reactapp", "name": "See on Github"},
+                {"href": "https://abhishekad7.github.io", "name": "Live"}
+            ]
+
+        },
+
+        {
+            title: "Live Cricker",
+            tools: ["Python"],
+            desc: "A Command Line app for getting live cricket matches updates. It shows notification at regular intervals.",
+            sss: [],
+            navs:[
+                {"href": "https://github.com/abhishekad7/live-cric", "name": "See on Github"},
+            ]
+
+        }
+    ]
+
+    constructor(props){
+        super(props);
+        this.addProject = this.addProject.bind(this);
+    }
+
+    addProject(obj, idx){
+
+        let project = <div className="project" key={idx}>
+            <div className="project-heading">
+                <span>{obj.title}</span>
+            </div>
+            <div className="project-tools">
+                {obj.tools.map((tool, idx) => {
+                    return (<span key={idx}>{tool}</span>)
+                })}
+            </div>
+            <div className="project-desc">
+                {obj.desc}
+            </div>
+            <div className="project-sss">
+                {obj.sss.map((src, idx) => {
+                    return (<img src={src} key={idx}/>)
+                })}
+            </div>
+            <div className="project-nav">
+                {obj.navs.map((nav, idx) => {
+                    return (<a target="_blank" rel="noopener noreferrer" href={nav.href}>{nav.name}</a>)
+                })}
+            </div>
+        </div>
+        //console.log(project);
+        return project;
+    }
+
+
     render() { 
         return (
             <center>
                 <div className="content-holder">
-
-
-                    <div className="project">
-                        <div className="project-heading">
-                            <span>Future Trivia</span>
-                        </div>
-                        <div className="project-tools">
-                            <span>Django</span>
-                            <span>Javascript</span>
-                        </div>
-                        <div className="project-desc">
-                            A Django based web app for onine quizzing and exams.
-                        </div>
-                        <div className="project-sss">
-                            <img src="/img/ft1.jpg" />
-                        </div>
-                        <div className="project-nav">
-                            <a target="_blank" href="https://github.com/abhishekad7/futureTrivia">See on Github</a>
-                            <a target="_blank" href="https://futuretrivia.pythonanywhere.com/">Live</a>
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="project-heading">
-                            <span>Kody Alarm: Coding Calendar</span>
-                        </div>
-                        <div className="project-tools">
-                            <span>Java</span>
-                            <span>Android Studio</span>
-                        </div>
-                        <div className="project-desc">
-                            An android app which shows you all upcoming programming contests on AtCoder, Codeforces and Codechef. User can set reminder for upcoming contest.
-                        </div>
-                        <div className="project-sss">
-                            <img src="/img/kody1.jpg" />
-                        </div>
-                        <div className="project-nav">
-                            <a target="_blank" href="https://github.com/abhishekad7/coding_calendar">See on Github</a>
-                            <a target="_blank" href="https://drive.google.com/open?id=1uPofiI-gQ2RiafWQLO3Q8CyZQRGk1lXe">Download App</a>
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="project-heading">
-                            <span>This Webapp</span>
-                        </div>
-                        <div className="project-tools">
-                            <span>ReactJS</span>
-                            <span>Javascript</span>
-                            <span>CSS</span>
-                        </div>
-                        <div className="project-desc">
-                            A ReactJS based web app. This web app is an image of me.
-                        </div>
-                        
-                        <div className="project-nav">
-                            <a target="_blank" href="https://github.com/abhishekad7/abhishekad7-reactapp">See on Github</a>
-                            <a target="_blank" href="https://abhishekad7.github.io">Live</a>
-                        </div>
-                    </div>
-
-
-
-                    <div className="project">
-                        <div className="project-heading">
-                            <span>Live Cricker</span>
-                        </div>
-                        <div className="project-tools">
-                            <span>Python</span>
-                        </div>
-                        <div className="project-desc">
-                            A Command Line app for getting live cricket matches updates. It shows notification at regular intervals.
-                        </div>
-                        
-                        <div className="project-nav">
-                            <a target="_blank" href="https://github.com/abhishekad7/live_cric">See on Github</a>
-                        </div>
-                    </div>
+                    
+                    {this.projects.map((project, idx) => {
+                        return this.addProject(project, idx);
+                    })}
 
                 </div>
 
